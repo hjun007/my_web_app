8035051a6f3534af12bf18991c01bee3ac01b765
hj <1498855201@qq.com>
2024-11-14 20:31:58 +0800

    OK

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# 检查是否存在.env文件
-if [ ! -f .env ]; then
-    echo "创建.env文件..."
-    cp .env.example .env
-fi
-
 if [ ! -f config.ini ]; then
     echo "创建config.ini文件..."
     cp config.ini.example config.ini
 fi
-
-# 加载环境变量
-source .env
 
 # 停止现有容器
 docker-compose down
